76720c1be74ca81a13093ff700f6ca17244ac173
Kevin <kz497@outlook.com>
2025-10-13 15:49:09 +1000

    fix: correct image path key in load_img causing KeyError

--- recognition/siameseNetwork48011343/dataset.py
+++ recognition/siameseNetwork48011343/dataset.py
@@ -33,7 +33,7 @@ class ISICDataset(Dataset):
 
         def load_img(sample):
             img = Image.open(os.path.join(self.image_root,
-                                sample['isic_id' + ".jpg"])).convert('RGB')
+                                sample['isic_id'] + ".jpg")).convert('RGB')
             return self.transform(img)
 
         return (load_img(anchor), load_img(positive), load_img(negative),
